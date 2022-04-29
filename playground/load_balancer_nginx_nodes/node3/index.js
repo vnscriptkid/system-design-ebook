b8828e80591bfa9c6a48ab696eb01d6e15bf6eec
Thanh Nguyen <vnscriptkid@gmail.com>
2022-04-29 23:46:23 +0700

    feat: play nginx nodes

---
+++ playground/load_balancer_nginx_nodes/node3/index.js
@@ -0,0 +1,8 @@
+var http = require('http')
+
+http
+  .createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end(`<h1>Server 3</h1>`)
+  })
+  .listen(8080)
