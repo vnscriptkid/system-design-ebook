a10d7acc875e9d1b2bfef429de4a1fca80ceb102
Thanh Nguyen <vnscriptkid@gmail.com>
2022-05-02 15:23:45 +0700

    fix: correct cli

--- playground/kafka/scripts/2-push-message.sh
+++ playground/kafka/scripts/2-push-message.sh
@@ -1,6 +1,3 @@
-docker exec -it kafka /opt/bitnami/kafka/bin/kafka-topics.sh \
-    --create \
-    --bootstrap-server localhost:9092 \
-    --replication-factor 1 \
-    --partitions 1 \
+docker exec -it kafka /opt/bitnami/kafka/bin/kafka-console-producer.sh \
+    --broker-list localhost:9092 \
     --topic test
